ef61cb7849518d9b161e0f9dfb7df21f6346806c
makomaiza3213 <makomaiza3213@gmail.com>
2021-07-30 18:43:02 -0300

    logo en login, y actualizaciones

--- Trabajo/src/windows/layout_sign_in.py
+++ Trabajo/src/windows/layout_sign_in.py
@@ -6,7 +6,10 @@ def login():
         diseño de la ventana de inicio de la aplicacion ,para inicio de Sesion y registro
     """
     sg.theme('DarkAmber')
-    layout_column = [[sg.Text("INGRESAR", size=(8, 1), font=("Fixedsys", 20))],
+    layout_column = [
+                     # [sg.Text("", font=("Fixedsys", 50))],
+                     [sg.Image("C:/Users/makom/Desktop/Facu 2021/PYTHON/Juego Mempy/Trabajo/src/images/mempy image logo 2.0.png")],
+                     [sg.Text("INGRESAR", size=(8, 1), font=("Fixedsys", 20))],
                      [sg.Text('USUARIO', font=("Fixedsys", 15))],
                      [sg.InputText(size=(15, 1), font=("Fixedsys", 12), key='-USERNAME-')],
                      [sg.Button('ENTRAR', size=(10, 1), font=("Fixedsys", 12), border_width=5)],
@@ -14,7 +17,8 @@ def login():
                      [sg.Text('¿ERES NUEVO?', font=("Fixedsys", 20))],
                      [sg.Button('REGISTRATE', size=(10, 1), font=("Fixedsys", 12), border_width=5)],
                      [sg.Text("", size=(0, 1))],
-                     [sg.Button(" SALIR ", font=("Fixedsys", 12), border_width=5, button_color=('black', 'red'))]]
+                     [sg.Button(" SALIR ", font=("Fixedsys", 12), border_width=5, button_color=('black', 'red'))]
+                    ]
 
     window = sg.Window("Welcome!", layout_column, element_justification='center', margins=(200, 150))
     return window
